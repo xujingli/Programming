6d7a3082a0f4d121f5b9eed6d6e7c64a0c3af82a
Jingli Xu <1220341948@qq.com>
2020-12-31 17:16:32 +0800

    fix: 二分

--- KickStart/2020/RoundA/P2678.cpp
+++ KickStart/2020/RoundA/P2678.cpp
@@ -6,10 +6,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool check(vector<int> &D, int N, int M, int step) {
+bool check(vector<int> &D, int M, int step) {
   // 判断最多能拿走M块石头的情况下，能否使得最小距离小于等于step
-  int lastPos = 0;  // 上一次的起始位置
-  for (int i = 0; i < N; i++) {
+  // 最后一块石头不能移动
+  int len = D.size();
+  int lastPos = 0;                     // 上一次的起始位置
+  for (int i = 0; i < len - 1; i++) {  // 从第一块到倒数第二块石头
     if (D[i] - lastPos < step) {
       if (M == 0) return false;
       M--;
@@ -18,8 +20,7 @@ bool check(vector<int> &D, int N, int M, int step) {
       continue;
     }
   }
-
-  return true;
+  return D[len - 1] - lastPos >= step;
 }
 
 int main() {
@@ -29,20 +30,35 @@ int main() {
   int L, N, M;
   cin >> L >> N >> M;
   // 分别表示起点到终点的距离，起点和终点之间的岩石数，以及组委会至多移走的岩石数。
-  vector<int> D(N + 1, 0);
+  if (N == 0) {
+    cout << L;
+    return 0;
+  }
+  vector<int> D(N, 0);
   for (int i = 0; i < N; i++) cin >> D[i];
   D.push_back(L);  // 最后一个位置
-
+  if (N == M) {
+    cout << L;
+    return 0;
+  }
   int left = 1, right = L;  // 最短跳跃距离的可能结果是[1, L]
   while (left < right) {
     int mid = left + (right - left) / 2;
     // 如果最短的跳跃距离可以为mid的话，说明可以更小或者就是mid
-    if (check(D, N + 1, M, mid))
+    if (check(D, M, mid))
       left = mid + 1;
     else
       right = mid - 1;
   }
-  cout << left << endl;
+  if (check(D, M, left - 1))
+    cout << left - 1 << endl;
+  else
+    cout << left << endl;
 
   return 0;
 }
+
+/*
+25 5 4
+2 11 14 17 21
+*/
