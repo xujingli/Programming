257b5f70052ccddd6d6d1a34baff4f0f6f393b01
Jingli Xu <1220341948@qq.com>
2021-01-28 21:18:45 +0800

    js promise 增加注释

--- JS_Related/promise.js
+++ JS_Related/promise.js
@@ -5,6 +5,11 @@ const PENDING = 'pending';
 const RESOLVED = 'resolved';
 const REJECTED = 'rejected';
 
+// Promise构造函数接受一个函数作为参数，该函数的两个参数分别是resolve和reject
+// resolve函数的作用是，将Promise对象的状态从“未完成”变为“成功”（即从 pending 变为 resolved），
+// 在异步操作成功时调用，并将异步操作的结果，作为参数传递出去；
+// reject函数的作用是，将Promise对象的状态从“未完成”变为“失败”（即从 pending 变为 rejected），
+// 在异步操作失败时调用，并将异步操作报出的错误，作为参数传递出去。
 function MyPromise(fn) {
   const that = this;
   that.value = null;
@@ -35,6 +40,10 @@ function MyPromise(fn) {
   }
 }
 
+// then方法可以接受两个回调函数作为参数。
+// 第一个回调函数是Promise对象的状态变为resolved时调用，
+// 第二个回调函数是Promise对象的状态变为rejected时调用。
+// 这两个函数都是可选的，不一定要提供。它们都接受Promise对象传出的值作为参数。
 MyPromise.prototype.then = function (onFulfilled, onRejected) {
   const that = this;
   onFulfilled = typeof onFulfilled === "Function" ? onFulfilled : v => v;
