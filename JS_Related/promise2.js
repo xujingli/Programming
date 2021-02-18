1dcc8de301ca6fa67452a8d013f4f35f1a901465
Jingli Xu <1220341948@qq.com>
2021-02-18 19:27:49 +0800

    fix: 手写promise 并且支持executor函数中含有异步函数

---
+++ JS_Related/promise2.js
@@ -0,0 +1,50 @@
+// 三个状态：PENDING、FULFILLED、REJECTED
+const PENDING = 'PENDING';
+const FULFILLED = 'FULFILLED';
+const REJECTED = 'REJECTED';
+
+class Promise {
+  constructor(executor) {
+    this.status = PENDING;
+    this.value = undefined;
+    this.reason = undefined;
+
+    this.onFulfilledCallbacks = [];
+    this.onRejectedCallbacks = [];
+
+    let resolve = (value) => {
+      if (this.status == PENDING) {
+        this.status = FULFILLED;
+        this.value = value;
+
+        this.onFulfilledCallbacks.forEach(fn => fn());
+      }
+    }
+
+    let reject = (reason) => {
+      if (this.status == PENDING) {
+        this.status = REJECTED;
+        this.reason = reason;
+        this.onRejectedCallbacks.forEach(fn => fn());
+      }
+    }
+
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    if (this.status == FULFILLED) {
+      onFulfilled(this.value);
+    } else if (this.status == REJECTED) {
+      onRejected(this.reason);
+    } else if (this.status == PENDING) {
+      // 先将onFulfilled 以及 onRejected函数（用户写的 then的两个参数）存起来 等待状态确定后，再依次调用callbacks
+      this.onFulfilledCallbacks.push(() => onFulfilled(this.value));
+      this.onRejectedCallbacks.push(() => onRejected(this.reason));
+    }
+  }
+}
